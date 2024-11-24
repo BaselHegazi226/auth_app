43fb4aae992027ab5ed170bcbddad1f6a2db071d
baselhegazi <baselmohammed226@gmail.com>
2024-11-25 00:00:46 +0200

    update success state function

--- lib/features/auth/presentation/views/widgets/verify_email_widgets/verify_email_view_body.dart
+++ lib/features/auth/presentation/views/widgets/verify_email_widgets/verify_email_view_body.dart
@@ -3,17 +3,13 @@ import 'dart:async';
 import 'package:auth_with_firebase_application/core/helper/constant.dart';
 import 'package:auth_with_firebase_application/core/utilities/custom_text_button.dart';
 import 'package:auth_with_firebase_application/core/utilities/custom_title.dart';
-import 'package:auth_with_firebase_application/features/auth/presentation/views/verify_email_view.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/verify_email_widgets/verifyMessage.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/verify_email_widgets/visibility_verification_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
-import '../../../../../../core/utilities/dialogState.dart';
 import '../../../../../../core/utilities/show_snack_bar.dart';
 import '../../../../../../core/utilities/text_styles.dart';
-import '../../../../../home/presentation/views/home_view.dart';
 import '../../../../../verify_email/presentation/views/widgets/custom_text.dart';
 import '../../../manager/auth_manager/auth_bloc.dart';
 
@@ -29,12 +25,6 @@ class _VerifyEmailViewBodyState extends State<VerifyEmailViewBody> {
   int counter = 30;
   Timer? timer;
   bool isButtonEnabled = false;
-
-  @override
-  void initState() {
-    super.initState();
-    // The counter function will only start when the button is pressed, so no need to call it here.
-  }
 
   void counterFun() {
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
@@ -82,43 +72,11 @@ class _VerifyEmailViewBodyState extends State<VerifyEmailViewBody> {
             const SizedBox(height: 16),
             BlocConsumer<AuthBloc, AuthStates>(listener: (context, state) {
               if (state is VerifyEmailSuccess) {
-                setState(() {
-                  isButtonEnabled = true;
-                });
-                customAwesomeDialog(
-                  context: context,
-                  dialogBackgroundColor: Colors.white,
-                  title: 'Success Send Verification Link',
-                  titleSize: 24,
-                  titleColor: Color(0xff00CA71),
-                  description: 'Check Your Inbox.',
-                  descriptionSize: 18,
-                  descriptionColor: Colors.grey,
-                  dialogBorderColor: Color(0xff00CA71),
-                  dialogState: 'success',
-                  dialogStateColor: Color(0xff00CA71),
-                  onSuccessPressed: () {
-                    GoRouter.of(context).push(
-                      HomeView.id,
-                      extra: VerifyEmailView.id,
-                    );
-                  },
-                  onCancelPressed: () {},
-                );
+                successState(context);
               } else if (state is VerifyEmailFailure) {
-                setState(() {
-                  counter = 0;
-                  isButtonEnabled = true;
-                });
-                showSnackBar(
-                  context,
-                  state.errorMessage,
-                  kErrorColor,
-                );
+                failureState(context, state);
               } else {
-                setState(() {
-                  isButtonEnabled = false;
-                });
+                loadedState();
               }
             }, builder: (context, state) {
               return CustomTextButton(
@@ -170,6 +128,35 @@ class _VerifyEmailViewBodyState extends State<VerifyEmailViewBody> {
           ],
         ),
       ),
+    );
+  }
+
+  void loadedState() {
+    setState(() {
+      isButtonEnabled = false;
+    });
+  }
+
+  void failureState(BuildContext context, VerifyEmailFailure state) {
+    setState(() {
+      counter = 0;
+      isButtonEnabled = true;
+    });
+    showSnackBar(
+      context,
+      state.errorMessage,
+      kErrorColor,
+    );
+  }
+
+  void successState(BuildContext context) {
+    setState(() {
+      isButtonEnabled = true;
+    });
+    showSnackBar(
+      context,
+      'Success real email enjoy!',
+      verifyColor,
     );
   }
 }
