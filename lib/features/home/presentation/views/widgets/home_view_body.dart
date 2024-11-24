df70181388c4a35e61b1b5b5f9ba7c14397cac71
baselhegazi <baselmohammed226@gmail.com>
2024-11-24 23:03:48 +0200

    update homeViewBody() to appear the success Awesome Dialog

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,17 +1,50 @@
+import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_widgets/sign_in_with_facebook.dart';
+import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_widgets/sign_in_with_google.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../../core/helper/constant.dart';
 import '../../../../../core/utilities/custom_big_icon.dart';
+import '../../../../../core/utilities/dialogState.dart';
 import '../../../../auth/presentation/views/widgets/auth_widgets/filter_section.dart';
 import 'home_view_button.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
+  @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  @override
+  void successAwesomeDialogInHome({required String successFrom}) {
+    String successFrom = 'Gmail';
+    switch (successFrom) {
+      case SignInWithFacebook.id:
+        successFrom = 'FaceBook';
+      case SignInWithGoogle.id:
+        successFrom = 'Google';
+      default:
+        successFrom = 'Gmail';
+    }
+    customAwesomeDialog(
+      context: context,
+      title: 'Success Sign In With $successFrom',
+      description: 'Welcome to the Home!',
+      dialogState: 'success',
+      onCancelPressed: () {
+        Navigator.of(context).pop(); // Close the dialog
+      },
+      onSuccessPressed: () {
+        Navigator.of(context).pop(); // Close the dialog
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     String navigateFrom = GoRouterState.of(context).extra as String;
+    successAwesomeDialogInHome(successFrom: navigateFrom);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kFocusColor.withOpacity(.05),
