58f7503eefed34509fc1f6c0e7a419bb22031a04
baselhegazi <baselmohammed226@gmail.com>
2024-12-08 23:09:54 +0200

    add phone image

--- lib/core/utilities/AssetsData.dart
+++ lib/core/utilities/AssetsData.dart
@@ -6,8 +6,8 @@ abstract class AssetsData {
   static const String kFacebookImage =
       'https://4.bp.blogspot.com/-Rm3giw7mj_8/VQLP1ZWAVwI/AAAAAAAAXw0/WPoJhWJu_aY/s1600/facebook-iOS-icon.png';
   static const String kPhoneImage =
-      'https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/dial_phone_telephone_call-1024.png';
-  static const String kPhoneOfflineImage = 'assets/images/green phone.webp';
+      'https://www.citypng.com/public/uploads/preview/white-outline-phone-telephone-icon-free-png-1164139392834ysjbztq5.png';
+  static const String kPhoneOfflineImage = 'assets/images/white phone.png';
   static const String kGoogleOfflineImage = 'assets/images/googleImage2.webp';
   static const String kMacOfflineImage = 'assets/images/appleImage.png';
   static const String kFacebookOfflineImage =
