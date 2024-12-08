c223328cf4615f2d7a1916f7cd6cb65bd3fb616e
baselhegazi <baselmohammed226@gmail.com>
2024-12-08 23:10:47 +0200

    add phone sign in

--- lib/features/auth/presentation/views/widgets/sign_in_widgets/other_sign_in.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_widgets/other_sign_in.dart
@@ -1,3 +1,6 @@
+import 'package:auth_with_firebase_application/core/helper/constant.dart';
+import 'package:auth_with_firebase_application/core/utilities/AssetsData.dart';
+import 'package:auth_with_firebase_application/core/utilities/custom_icon_button.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_widgets/sign_in_with_facebook.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_widgets/sign_in_with_google.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +25,14 @@ class OtherSignIn extends StatelessWidget {
         // SizedBox(width: 4),
         SignInWithFacebook(
           isEnableFacebookButton: isEnableButton,
+        ),
+        CustomIconButton(
+          textColor: kPhoneColor,
+          imageColor: kPhoneColor,
+          image: AssetsData.kPhoneImage,
+          errorLoadImage: AssetsData.kPhoneOfflineImage,
+          onPressed: () {},
+          title: 'Phone',
         ),
       ],
     );
