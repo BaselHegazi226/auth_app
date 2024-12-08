a03c46d8329bf464372781edee5c195068c44e40
baselhegazi <baselmohammed226@gmail.com>
2024-12-08 23:00:28 +0200

    add phone image , offline phone image

--- lib/core/utilities/AssetsData.dart
+++ lib/core/utilities/AssetsData.dart
@@ -5,6 +5,9 @@ abstract class AssetsData {
       'https://www.dvc.edu/faculty-staff/online-accessibility/images/Mac%20icon.png';
   static const String kFacebookImage =
       'https://4.bp.blogspot.com/-Rm3giw7mj_8/VQLP1ZWAVwI/AAAAAAAAXw0/WPoJhWJu_aY/s1600/facebook-iOS-icon.png';
+  static const String kPhoneImage =
+      'https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/dial_phone_telephone_call-1024.png';
+  static const String kPhoneOfflineImage = 'assets/images/green phone.webp';
   static const String kGoogleOfflineImage = 'assets/images/googleImage2.webp';
   static const String kMacOfflineImage = 'assets/images/appleImage.png';
   static const String kFacebookOfflineImage =
