7bf8460c0ef1c2b2a92c470ca6f43f8b4b93d028
baselhegazi <baselmohammed226@gmail.com>
2024-12-15 14:25:11 +0200

    return her app bar

--- lib/features/auth/presentation/views/sign_in_phone_num_view.dart
+++ lib/features/auth/presentation/views/sign_in_phone_num_view.dart
@@ -1,12 +1,26 @@
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/sign_in_with_phone_num_view_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/utilities/text_styles.dart';
+
 class SignInPhoneNumView extends StatelessWidget {
-  static String id = '/verifyPhoneNumView';
+  static String id = '/signInPhoneNumView';
   const SignInPhoneNumView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SignInWithPhoneNumViewBody();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title: Text(
+          'Phone Number Sign In',
+          style: Styles.textStyleFun(
+            color: Colors.green,
+            size: 24,
+          ),
+        ),
+      ),
+      body: SignInWithPhoneNumViewBody(),
+    );
   }
 }
