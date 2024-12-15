c3a77262c9ee856930b07478516bc7580a42577a
baselhegazi <baselmohammed226@gmail.com>
2024-12-15 14:05:37 +0200

    update it

--- lib/features/auth/presentation/views/verify_phone_num_view.dart
+++ lib/features/auth/presentation/views/verify_phone_num_view.dart
@@ -1,3 +1,4 @@
+import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/verify_phone_num_widgets/verify_phone_num_view_body.dart';
 import 'package:flutter/material.dart';
 
 class VerifyPhoneNumView extends StatelessWidget {
@@ -6,10 +7,6 @@ class VerifyPhoneNumView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Verify Phone Number'),
-      ),
-    );
+    return VerifyPhoneNumViewBody();
   }
 }
