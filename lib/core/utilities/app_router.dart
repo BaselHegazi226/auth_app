031dc892419185ecb32e0456ba197cac040542c2
baselhegazi <baselmohammed226@gmail.com>
2024-12-15 14:00:12 +0200

    update some importing

--- lib/core/utilities/app_router.dart
+++ lib/core/utilities/app_router.dart
@@ -3,9 +3,10 @@ import 'package:auth_with_firebase_application/features/auth/presentation/views/
 import 'package:auth_with_firebase_application/features/auth/presentation/views/sign_in_view.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/sing_up_view.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/verify_email_view.dart';
-import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_widgets/sign_in_with_phone.dart';
 import 'package:auth_with_firebase_application/features/home/presentation/views/home_view.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../features/auth/presentation/views/verify_phone_num_view.dart';
 
 abstract class AppRouter {
   static final router = GoRouter(routes: [
