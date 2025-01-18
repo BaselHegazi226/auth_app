5bfca04494d83d9fc8b457ace48f2dbdc0e9767e
baselhegazi <baselmohammed226@gmail.com>
2025-01-18 14:44:38 +0200

    change the center color of filter section & add custom  big icon to stack & add change the column widget of custom text form field to center widget & suffixIcon to custom text form field

--- lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/sign_in_with_phone_num_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/sign_in_with_phone_num_view_body.dart
@@ -1,15 +1,38 @@
+import 'package:auth_with_firebase_application/core/utilities/custom_big_icon.dart';
+import 'package:auth_with_firebase_application/core/utilities/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 import '../auth_widgets/filter_section.dart';
 
 class SignInWithPhoneNumViewBody extends StatelessWidget {
-  const SignInWithPhoneNumViewBody({super.key});
+  SignInWithPhoneNumViewBody({super.key});
+  final TextEditingController phoneController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    //center color Color.fromRGBO(144, 238, 144, 1),
     return Stack(
       children: [
-        FilterSection(), // Fixed background
+        FilterSection(
+          leftColor: Color.fromRGBO(174, 213, 129, 1),
+          centerColor: Colors.white,
+          rightColor: Color.fromRGBO(102, 187, 106, 1),
+        ),
+        CustomBigIcon(
+          iconData: Icons.phone,
+        ),
+        Center(
+          child: CustomTextFormField(
+            hintText: 'Phone Number',
+            suffixIcon: Icon(
+              Icons.phone,
+              color: Colors.green,
+            ),
+            textEditingController: phoneController,
+            keyboardType: TextInputType.phone,
+            textColor: Colors.green,
+          ),
+        ) // Fixed background,)
       ],
     );
   }
