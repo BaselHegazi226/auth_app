19f2d4d398903b130839d8ffae7db5822127d674
baselhegazi <baselmohammed226@gmail.com>
2025-01-19 20:51:15 +0200

    create const functions class

---
+++ lib/core/helper/const_functions.dart
@@ -0,0 +1,60 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+import 'const_variables.dart';
+
+class ConstFunctions {
+  static Tuple2<IconData, Color> iconDataAndColorFun(
+      {required bool condition}) {
+    IconData iconData;
+    Color color;
+    if (condition) {
+      iconData = kEnableIconData;
+      color = kEmailFocusColor;
+    } else {
+      iconData = kDisableIconData;
+      color = kDisableColor;
+    }
+    return Tuple2(iconData, color);
+  }
+
+  static DialogType dialogType({required final String type}) {
+    switch (type) {
+      case 'success':
+        return DialogType.success;
+      case 'error':
+        return DialogType.error;
+      case 'info':
+        return DialogType.info;
+      case 'infoReverse':
+        return DialogType.infoReverse;
+      case 'noHeader':
+        return DialogType.noHeader;
+      case 'question':
+        return DialogType.question;
+      case 'warning':
+        return DialogType.warning;
+      default:
+        return DialogType.success;
+    }
+  }
+
+//return enable color if the condition true else disable color
+  static Color colorBackFun({required bool condition, required String word}) {
+    return condition ? colorByWordFun(word: word) : kDisableColor;
+  }
+
+  static Color colorByWordFun({required String word}) {
+    Color backcolor = Colors.white;
+    switch (word) {
+      case 'phone':
+        backcolor = kPhoneFocusColor;
+        break;
+      case 'email':
+        backcolor = kEmailFocusColor;
+        break;
+    }
+    return backcolor;
+  }
+}
