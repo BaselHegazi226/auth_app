642ff6f4853f554beb39d0f627193493108cd64e
baselhegazi <baselmohammed226@gmail.com>
2025-01-19 23:18:00 +0200

    create Otp View and add her id

---
+++ lib/features/auth/presentation/views/otp_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OtpView extends StatelessWidget {
+  const OtpView({super.key});
+  static String id = '/OtpView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
