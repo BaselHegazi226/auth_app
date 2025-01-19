6745cfc87b22faa6a49c811aee024d90ee18cfb7
baselhegazi <baselmohammed226@gmail.com>
2025-01-19 23:29:55 +0200

    import import 'package:auth_with_firebase_application/core/utilities/text_styles.dart'; import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart';

--- lib/features/auth/presentation/views/otp_view.dart
+++ lib/features/auth/presentation/views/otp_view.dart
@@ -1,3 +1,5 @@
+import 'package:auth_with_firebase_application/core/utilities/text_styles.dart';
+import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart';
 import 'package:flutter/material.dart';
 
 class OtpView extends StatelessWidget {
@@ -5,6 +7,20 @@ class OtpView extends StatelessWidget {
   static String id = '/OtpView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'OTP View',
+          style: Styles.textStyleFun(
+            color: Colors.grey,
+            size: 24,
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+        ),
+      ),
+      body: OtpViewBody(),
+    );
   }
 }
