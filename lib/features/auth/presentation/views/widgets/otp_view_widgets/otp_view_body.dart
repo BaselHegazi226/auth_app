1be6d22643ff434f9a391cf14096a6d3857d3a4f
baselhegazi <baselmohammed226@gmail.com>
2025-01-20 00:15:05 +0200

    add children of otp view body

--- lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
+++ lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
@@ -1,10 +1,66 @@
+import 'package:auth_with_firebase_application/core/helper/const_variables.dart';
+import 'package:auth_with_firebase_application/core/helper/custom_flexible_widget.dart';
+import 'package:auth_with_firebase_application/core/utilities/custom_big_icon.dart';
+import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/auth_widgets/filter_section.dart';
 import 'package:flutter/material.dart';
 
-class OtpViewBody extends StatelessWidget {
+import 'otp_item.dart';
+
+class OtpViewBody extends StatefulWidget {
   const OtpViewBody({super.key});
 
   @override
+  State<OtpViewBody> createState() => _OtpViewBodyState();
+}
+
+class _OtpViewBodyState extends State<OtpViewBody> {
+  final TextEditingController otp1 = TextEditingController();
+  final TextEditingController otp2 = TextEditingController();
+  final TextEditingController otp3 = TextEditingController();
+  final TextEditingController otp4 = TextEditingController();
+  final TextEditingController otp5 = TextEditingController();
+  final TextEditingController otp6 = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Stack(
+      children: [
+        FilterSection(
+          rightColor: kOtpRightColor,
+          centerColor: kOtpCenterColor,
+          leftColor: kOtpLeftColor,
+        ),
+        const CustomBigIcon(iconData: Icons.verified),
+        CustomFlexibleWidget(
+          widget: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OtpItem(
+                otpController: otp1,
+              ),
+              const SizedBox(width: 6),
+              OtpItem(
+                otpController: otp2,
+              ),
+              const SizedBox(width: 6),
+              OtpItem(
+                otpController: otp3,
+              ),
+              const SizedBox(width: 6),
+              OtpItem(
+                otpController: otp4,
+              ),
+              const SizedBox(width: 6),
+              OtpItem(
+                otpController: otp5,
+              ),
+              const SizedBox(width: 6),
+              OtpItem(
+                otpController: otp6,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
