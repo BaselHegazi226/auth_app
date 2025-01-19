f83848dd3fcb0e0819176368c697c5d13e0e99a4
baselhegazi <baselmohammed226@gmail.com>
2025-01-19 23:29:24 +0200

    make GoRouter.of(context).push(                OtpView.id,              ); temporary

--- lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/phone_button.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/phone_button.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../../core/helper/const_variables.dart';
 import '../../../../../../core/utilities/custom_text_button.dart';
 import '../../../../../../core/utilities/custom_title.dart';
+import '../../otp_view.dart';
 
 class PhoneButton extends StatelessWidget {
   const PhoneButton({super.key});
@@ -10,7 +12,11 @@ class PhoneButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomTextButton(
-      onPressed: () {},
+      onPressed: () {
+        GoRouter.of(context).push(
+          OtpView.id,
+        );
+      },
       backgroundColor: kPhoneLeftColor,
       shadowColor: kPhoneLeftColor,
       child: CustomTitle(
