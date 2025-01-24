bb89d4f6e3a611433e4a5b9f68668a4dfef720fb
baselhegazi <baselmohammed226@gmail.com>
2025-01-24 08:35:14 +0200

    comment it

--- lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_item.dart
+++ lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_item.dart
@@ -1,22 +1,22 @@
-import 'package:auth_with_firebase_application/core/utilities/custom_text_form_field_otp.dart';
-import 'package:flutter/material.dart';
-
-import '../../../../../../core/helper/const_variables.dart';
-
-class OtpItem extends StatelessWidget {
-  const OtpItem({
-    super.key,
-    required this.otpController,
-  });
-  final TextEditingController otpController;
-  @override
-  Widget build(BuildContext context) {
-    return CustomTextFormFieldOtp(
-      backgroundColor: Colors.black.withOpacity(.11),
-      hintText: '',
-      textEditingController: otpController,
-      keyboardType: TextInputType.number,
-      generalColor: kOtpCenterColor,
-    );
-  }
-}
+// import 'package:auth_with_firebase_application/core/utilities/custom_text_form_field_otp.dart';
+// import 'package:flutter/material.dart';
+//
+// import '../../../../../../core/helper/const_variables.dart';
+//
+// class OtpItem extends StatelessWidget {
+//   const OtpItem({
+//     super.key,
+//     required this.otpController,
+//   });
+//   final TextEditingController otpController;
+//   @override
+//   Widget build(BuildContext context) {
+//     return CustomTextFormFieldOtp(
+//       backgroundColor: Colors.black.withOpacity(.11),
+//       hintText: '',
+//       textEditingController: otpController,
+//       keyboardType: TextInputType.number,
+//       generalColor: kOtpCenterColor,
+//     );
+//   }
+// }
