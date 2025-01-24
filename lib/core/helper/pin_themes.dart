cb585ddb0819434d7247dcb9a84788975f15355c
baselhegazi <baselmohammed226@gmail.com>
2025-01-24 07:18:42 +0200

    create HeightWidthPinPutOtp class to make the height , width of the otp PinPut is dynamic & make the PinThemes class to make the customPinTheme is reusable

---
+++ lib/core/helper/pin_themes.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class HeightWidthPinPutOtp {
+  static Tuple2<double, double> backSizeFun(BuildContext context) {
+    final double height = MediaQuery.sizeOf(context).height * .07;
+    final double width = MediaQuery.sizeOf(context).width * .15;
+    return Tuple2(height, width);
+  }
+}
+
+class PinThemes {
+  static PinTheme customPinTheme(
+    BuildContext context, {
+    required double height,
+    required double width,
+    required double fontSize,
+    required Color textColor,
+    required Color backgroundColor,
+    required Color borderColor,
+    required double borderRadius,
+  }) {
+    return PinTheme(
+      height: height,
+      width: width,
+      textStyle: TextStyle(
+        fontSize: fontSize,
+        color: textColor,
+        fontWeight: FontWeight.w600,
+      ),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        border: Border.all(
+          width: 2,
+          color: borderColor,
+        ),
+        borderRadius: BorderRadius.circular(
+          borderRadius,
+        ),
+      ),
+    );
+  }
+}
