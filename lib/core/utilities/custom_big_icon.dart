7bf7b39c3eccea5009556af0b58fef9e941e1cc9
baselhegazi <baselmohammed226@gmail.com>
2025-01-25 13:44:43 +0200

    add iconColor parameter to it

--- lib/core/utilities/custom_big_icon.dart
+++ lib/core/utilities/custom_big_icon.dart
@@ -4,19 +4,21 @@ class CustomBigIcon extends StatelessWidget {
   const CustomBigIcon({
     super.key,
     required this.iconData,
+    this.iconColor = Colors.white,
   });
   final IconData iconData;
+  final Color iconColor;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
-          height: MediaQuery.of(context).size.height * .5,
+          height: MediaQuery.of(context).size.width * .7,
           child: Icon(
             iconData,
             size: 150,
-            color: Colors.white.withOpacity(.5),
+            color: iconColor,
           ),
         ),
       ],
