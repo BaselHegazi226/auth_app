fea18360a2a7d4c51d7a7c65e1b7a1ac43314980
baselhegazi <baselmohammed226@gmail.com>
2025-01-25 13:44:12 +0200

    update some widgets to make the widgets is responsive and make the arrangement of the view is icon then otp then the button

--- lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
+++ lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:auth_with_firebase_application/core/helper/const_variables.dart';
 import 'package:auth_with_firebase_application/core/helper/custom_flexible_widget.dart';
 import 'package:auth_with_firebase_application/core/utilities/custom_big_icon.dart';
+import 'package:auth_with_firebase_application/core/utilities/custom_text_form_field_otp.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/auth_widgets/filter_section.dart';
-import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/otp_view_widgets/otp_section.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../core/utilities/custom_text_button.dart';
@@ -29,29 +29,69 @@ class _OtpViewBodyState extends State<OtpViewBody> {
         children: [
           FilterSection(
             rightColor: kOtpRightColor,
-            centerColor: kOtpCenterColor,
             leftColor: kOtpLeftColor,
+            centerColor: kOtpCenterColor,
           ),
           Align(
-            alignment: Alignment(0, -.8),
-            child: const CustomBigIcon(iconData: Icons.verified),
-          ),
-          Center(
-            child: OtpSection(),
-          ),
-          Align(
-            alignment: Alignment(0, -.4),
-            child: CustomTextButton(
-              onPressed: () {},
-              backgroundColor: Colors.grey,
-              shadowColor: Colors.grey.withAlpha(128),
-              child: CustomTitle(
-                title: 'Verify',
-              ),
+            alignment: Alignment(0, -.75),
+            child: CustomBigIcon(
+              iconData: Icons.phone,
+              iconColor: Colors.white,
             ),
           ),
+          Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 32,
+                children: [
+                  CustomTextFormFieldOtpPinPut(),
+                  CustomTextButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.grey,
+                    shadowColor: Colors.grey.withAlpha(128),
+                    child: CustomTitle(
+                      title: 'Verify',
+                    ),
+                  ),
+                ],
+              )),
         ],
       ),
     );
+
+    // return Center(
+    //   child: Stack(
+    //     children: [
+    //       FilterSection(
+    //         rightColor: kOtpRightColor,
+    //         centerColor: kOtpCenterColor,
+    //         leftColor: kOtpLeftColor,
+    //       ),
+    //       Align(
+    //         alignment: Alignment(0, -1),
+    //         child: const CustomBigIcon(
+    //           iconData: Icons.verified,
+    //           iconColor: Colors.grey,
+    //         ),
+    //       ),
+    //       Align(
+    //         alignment: Alignment(0, 0),
+    //         child: CustomTextFormFieldOtpPinPut(),
+    //       ),
+    //       Align(
+    //         alignment: Alignment(0, -.05),
+    //         child: CustomTextButton(
+    //           onPressed: () {},
+    //           backgroundColor: Colors.grey,
+    //           shadowColor: Colors.grey.withAlpha(128),
+    //           child: CustomTitle(
+    //             title: 'Verify',
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+//    );
   }
 }
