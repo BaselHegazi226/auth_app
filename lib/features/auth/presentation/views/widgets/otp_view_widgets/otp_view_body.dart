fc81b2b025063215e7bcf7a5947674758713937c
baselhegazi <baselmohammed226@gmail.com>
2025-01-25 13:49:18 +0200

    add otpSection() widget

--- lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
+++ lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:auth_with_firebase_application/core/helper/const_variables.dart';
 import 'package:auth_with_firebase_application/core/helper/custom_flexible_widget.dart';
 import 'package:auth_with_firebase_application/core/utilities/custom_big_icon.dart';
-import 'package:auth_with_firebase_application/core/utilities/custom_text_form_field_otp.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/auth_widgets/filter_section.dart';
+import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/otp_view_widgets/otp_section.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../core/utilities/custom_text_button.dart';
@@ -45,7 +45,7 @@ class _OtpViewBodyState extends State<OtpViewBody> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 spacing: 32,
                 children: [
-                  CustomTextFormFieldOtpPinPut(),
+                  OtpSection(),
                   CustomTextButton(
                     onPressed: () {},
                     backgroundColor: Colors.grey,
