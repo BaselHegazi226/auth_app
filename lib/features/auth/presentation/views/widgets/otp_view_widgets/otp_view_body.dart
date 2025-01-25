8429bc62d09ada0e32944bc37e2897f80087927d
baselhegazi <baselmohammed226@gmail.com>
2025-01-25 16:58:08 +0200

    make some changes 1- from (alignment: Alignment(0, -.75),) to (alignment: Alignment(0, -.85),) for (CustomBigIcon), 2- add  CustomTextFormFieldOtpPinPut(        pinCount: 6,), instead of OtpSection()

--- lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
+++ lib/features/auth/presentation/views/widgets/otp_view_widgets/otp_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:auth_with_firebase_application/core/helper/const_variables.dart';
 import 'package:auth_with_firebase_application/core/helper/custom_flexible_widget.dart';
 import 'package:auth_with_firebase_application/core/utilities/custom_big_icon.dart';
+import 'package:auth_with_firebase_application/core/utilities/custom_text_form_field_otp.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/auth_widgets/filter_section.dart';
-import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/otp_view_widgets/otp_section.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../core/utilities/custom_text_button.dart';
@@ -33,10 +33,10 @@ class _OtpViewBodyState extends State<OtpViewBody> {
             centerColor: kOtpCenterColor,
           ),
           Align(
-            alignment: Alignment(0, -.75),
+            alignment: Alignment(0, -.85),
             child: CustomBigIcon(
               iconData: Icons.phone,
-              iconColor: Colors.white,
+              iconColor: Colors.white.withAlpha(200),
             ),
           ),
           Align(
@@ -45,7 +45,9 @@ class _OtpViewBodyState extends State<OtpViewBody> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 spacing: 32,
                 children: [
-                  OtpSection(),
+                  CustomTextFormFieldOtpPinPut(
+                    pinCount: 6,
+                  ),
                   CustomTextButton(
                     onPressed: () {},
                     backgroundColor: Colors.grey,
