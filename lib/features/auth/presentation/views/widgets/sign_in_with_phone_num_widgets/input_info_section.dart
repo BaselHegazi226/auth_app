282dfae52780c886c16321257f5d7b025d377770
baselhegazi <baselmohammed226@gmail.com>
2025-01-27 14:17:30 +0200

    build the bloc with the widget bloc consumer which provide build parameter and listener parameter

--- lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/input_info_section.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/input_info_section.dart
@@ -1,6 +1,7 @@
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/phone_number_view_text_section.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/phone_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../../../core/helper/const_variables.dart';
@@ -36,49 +37,54 @@ class _InputInfoSectionState extends State<InputInfoSection> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      autovalidateMode: autoValidateMode,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          PhoneNumberViewTextSection(
-            phoneFocusNode: phoneFocusNode,
+    return BlocConsumer(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Form(
+          key: _formKey,
+          autovalidateMode: autoValidateMode,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              PhoneNumberViewTextSection(
+                phoneFocusNode: phoneFocusNode,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              PhoneTextFormField(
+                phoneController: phoneEditingController,
+                phoneFocusNode: phoneFocusNode,
+                validator: (value) {
+                  String phone = "+2${value!.trim()}";
+                  if (phone.length != 13 && !phone.startsWith('+20')) {
+                    return 'Invalid Number';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              CustomTextButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    GoRouter.of(context).push(
+                      OtpView.id,
+                    );
+                  }
+                },
+                backgroundColor: kPhoneLeftColor,
+                shadowColor: kPhoneLeftColor,
+                child: CustomTitle(
+                  title: 'Verify Phone Number',
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 16,
-          ),
-          PhoneTextFormField(
-            phoneController: phoneEditingController,
-            phoneFocusNode: phoneFocusNode,
-            validator: (value) {
-              String phone = "+2${value!.trim()}";
-              if (phone.length != 13 && !phone.startsWith('+20')) {
-                return 'Invalid Number';
-              } else {
-                return null;
-              }
-            },
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          CustomTextButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                GoRouter.of(context).push(
-                  OtpView.id,
-                );
-              }
-            },
-            backgroundColor: kPhoneLeftColor,
-            shadowColor: kPhoneLeftColor,
-            child: CustomTitle(
-              title: 'Verify Phone Number',
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
