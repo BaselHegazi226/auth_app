df50a9b439bfaa1d48f58dda1b6ce9fb059c75e0
baselhegazi <baselmohammed226@gmail.com>
2025-01-29 17:29:57 +0200

    1- make the autoValidateMode variable from (final AutovalidateMode autoValidateMode = AutovalidateMode.disabled;) to (AutovalidateMode autoValidateMode = AutovalidateMode.disabled;) . 2- add type for BlocConsumer <AuthSocialBloc, AuthStates> 3- add body of listener . 3.1- add successStateFun(). 3.2 add failureStateFun(). 3.3 add loadedStateFun(). 4- change the validator to  egyptianPhoneRegex. 5- change the body of onPressed for CustomTextButton . 5.1 change the style of child of button from ( CustomTitle(title: 'Verify Phone Number',),) to (isEnableButton ? CustomCircleIndicator(): CustomTitle(title: 'Send',),)

--- lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/input_info_section.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/input_info_section.dart
@@ -1,3 +1,5 @@
+import 'package:auth_with_firebase_application/core/utilities/custom_circle_indicator.dart';
+import 'package:auth_with_firebase_application/features/auth/presentation/manager/auth_manager/auth_bloc.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/phone_number_view_text_section.dart';
 import 'package:auth_with_firebase_application/features/auth/presentation/views/widgets/sign_in_with_phone_num_widgets/phone_text_form_field.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +9,7 @@ import 'package:go_router/go_router.dart';
 import '../../../../../../core/helper/const_variables.dart';
 import '../../../../../../core/utilities/custom_text_button.dart';
 import '../../../../../../core/utilities/custom_title.dart';
+import '../../../../../../core/utilities/show_snack_bar.dart';
 import '../../otp_view.dart';
 
 class InputInfoSection extends StatefulWidget {
@@ -20,7 +23,8 @@ class _InputInfoSectionState extends State<InputInfoSection> {
   final TextEditingController phoneEditingController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   final FocusNode phoneFocusNode = FocusNode();
-  final AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  bool isEnableButton = false;
   @override
   void initState() {
     phoneFocusNode.addListener(() {
@@ -37,8 +41,16 @@ class _InputInfoSectionState extends State<InputInfoSection> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer(
-      listener: (context, state) {},
+    return BlocConsumer<AuthSocialBloc, AuthStates>(
+      listener: (context, state) {
+        if (state is SignInWithPhoneSuccess) {
+          successStateFun(context, state);
+        } else if (state is SignInWithPhoneFailure) {
+          failureStateFun(state);
+        } else {
+          loadedStateFun();
+        }
+      },
       builder: (context, state) {
         return Form(
           key: _formKey,
@@ -56,9 +68,12 @@ class _InputInfoSectionState extends State<InputInfoSection> {
                 phoneController: phoneEditingController,
                 phoneFocusNode: phoneFocusNode,
                 validator: (value) {
-                  String phone = "+2${value!.trim()}";
-                  if (phone.length != 13 && !phone.startsWith('+20')) {
-                    return 'Invalid Number';
+                  final RegExp egyptianPhoneRegex =
+                      RegExp(r'^(010|011|012|015)[0-9]{8}$');
+                  if (value!.isEmpty) {
+                    return 'Phone number is required';
+                  } else if (!egyptianPhoneRegex.hasMatch(value)) {
+                    return 'Enter Correct Phone number';
                   } else {
                     return null;
                   }
@@ -70,21 +85,61 @@ class _InputInfoSectionState extends State<InputInfoSection> {
               CustomTextButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
-                    GoRouter.of(context).push(
-                      OtpView.id,
+                    BlocProvider.of<AuthSocialBloc>(context).add(
+                      SignInWithPhoneEvent(
+                        phoneNumber: '+2${phoneEditingController.text.trim()}',
+                      ),
                     );
+                    FocusScope.of(context).unfocus();
+                  } else {
+                    autoValidateMode = AutovalidateMode.always;
                   }
                 },
                 backgroundColor: kPhoneLeftColor,
                 shadowColor: kPhoneLeftColor,
-                child: CustomTitle(
-                  title: 'Verify Phone Number',
-                ),
+                child: isEnableButton
+                    ? CustomCircleIndicator()
+                    : CustomTitle(
+                        title: 'Send',
+                      ),
               ),
             ],
           ),
         );
       },
     );
+  }
+
+  void successStateFun(BuildContext context, SignInWithPhoneSuccess state) {
+    GoRouter.of(context).push(
+      OtpView.id,
+      extra: state.verifyId,
+    );
+    showSnackBar(context, 'Success sign up ', kEmailFocusColor);
+    onResetFun();
+  }
+
+  void onResetFun() {
+    setState(() {
+      autoValidateMode = AutovalidateMode.disabled;
+      isEnableButton = true;
+    });
+  }
+
+  void failureStateFun(SignInWithPhoneFailure state) {
+    setState(
+      () {
+        isEnableButton = true;
+        autoValidateMode =
+            AutovalidateMode.always; // Trigger validation after an error
+        _formKey.currentState?.validate(); // Trigger form re-validation
+      },
+    );
+  }
+
+  void loadedStateFun() {
+    setState(() {
+      isEnableButton = false;
+    });
   }
 }
