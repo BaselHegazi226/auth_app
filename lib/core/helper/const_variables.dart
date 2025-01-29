ab36487eefb8aafc185f3ad88d86b09fc942c294
baselhegazi <baselmohammed226@gmail.com>
2025-01-29 17:33:36 +0200

    change the color of phone and otp

--- lib/core/helper/const_variables.dart
+++ lib/core/helper/const_variables.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 
+const IconData kDisableIconData = Icons.check_box_outline_blank_rounded;
+const IconData kEnableIconData = Icons.check_box_rounded;
+
 Color kDisableColor = Colors.grey;
 Color kEmailFocusColor = const Color.fromRGBO(244, 143, 177, 1);
 Color kPasswordFocusColor = const Color.fromRGBO(244, 143, 177, 1);
+
 Color kPhoneFocusColor = const Color.fromRGBO(102, 187, 106, 1);
 Color kDefaultForgetPassColor = Colors.grey;
 final Color kFocusForgetPassColor = Colors.grey.shade600;
-const IconData kDisableIconData = Icons.check_box_outline_blank_rounded;
-const IconData kEnableIconData = Icons.check_box_rounded;
+
 final Color kFacebookColor = Color(0xff3F67CA);
 final Color kPhoneColor = Color(0xff62BB27);
 final Color kGoogleColor = Colors.black;
 final Color kSocialColor = Colors.grey.shade500;
 final Color kMacColor = Colors.black.withOpacity(.7);
+
 const Color kFilledTextFormFieldColor = Colors.grey;
 const kErrorColor = Colors.red;
+
 const Color kFilterCenterColor = Color.fromRGBO(255, 204, 128, 1);
 const Color kFilterRightColor = Color.fromRGBO(206, 147, 216, 1);
 const Color kFilterLeftColor = Color.fromRGBO(244, 143, 177, 1);
+
 const Color kTitleDialogColor = Color(0xff00CA71);
 const Color kDialogStateColor = Color(0xff00CA71);
 const Color kDialogBorderColor = Colors.white;
 const Color kDialogBackgroundColor = Colors.white;
 const Color kDescriptionColor = Colors.grey;
+
 final Color kPhoneLeftColor = Color.fromRGBO(102, 187, 106, 1);
-final Color kPhoneCenterColor = Colors.white;
+final Color kPhoneCenterColor = Color.fromRGBO(138, 200, 117, 1);
 final Color kPhoneRightColor = Color.fromRGBO(174, 213, 129, 1);
-final Color kOtpRightColor = Color.fromRGBO(174, 174, 174, 1); // Light grey
-final Color kOtpCenterColor = Colors.white;
-final Color kOtpLeftColor = Color.fromRGBO(102, 102, 102, 1); // Darker grey
+
+final Color kOtpRightColor = Color.fromRGBO(102, 187, 106, 1);
+final Color kOtpCenterColor = Color.fromRGBO(138, 200, 117, 1);
+final Color kOtpLeftColor = Color.fromRGBO(102, 187, 106, 1);
